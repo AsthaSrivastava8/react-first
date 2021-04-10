55cbbb3c53c2050db2b1182a8569e53cae305b2e
Astha <srivastavastha811@gmail.com>
2021-04-11 02:17:55 +0530

    Added first component

---
+++ my-first-app/src/components/counter.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>Hello World again</h1>
+        <button>Increment</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
