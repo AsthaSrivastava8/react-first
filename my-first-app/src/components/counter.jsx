c2a68e72d6a274ad888967c3732b5b583481f438
Astha <srivastavastha811@gmail.com>
2021-04-11 02:52:44 +0530

    Added classes

--- my-first-app/src/components/counter.jsx
+++ my-first-app/src/components/counter.jsx
@@ -1,14 +1,34 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {};
+  state = {
+    count: 0,
+    address: {
+      street: "Baker Street",
+      apartment: "221B",
+    },
+    imageURL: "https://picsum.photos/300",
+  };
+
+  styles = {
+    fontSize: 10,
+    fontWeight: "bold",
+  };
   render() {
     return (
       <div>
-        <h1>Hello World again</h1>
-        <button>Increment</button>
+        {/* <img src={this.state.imageURL} alt="Not available" /> */}
+        <span style={this.styles} className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
