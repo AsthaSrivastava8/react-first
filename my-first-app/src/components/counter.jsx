fe72cc3cf727bc731e5ef3c1577d296e26d68bf8
Astha <srivastavastha811@gmail.com>
2021-04-11 15:54:54 +0530

    Lists

--- my-first-app/src/components/counter.jsx
+++ my-first-app/src/components/counter.jsx
@@ -8,28 +8,41 @@ class Counter extends Component {
       apartment: "221B",
     },
     imageURL: "https://picsum.photos/300",
+    tags: ["tag1", "tag2", "tag3"],
   };
 
   styles = {
     fontSize: 10,
     fontWeight: "bold",
   };
+
   render() {
     return (
       <div>
         {/* <img src={this.state.imageURL} alt="Not available" /> */}
-        <span style={this.styles} className="badge badge-primary m-2">
-          {this.formatCount()}
-        </span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        {/* <span style={this.styles} className={this.getBadgeClasses()}> */}
+          {/* {this.formatCount()} */}
+        {/* </span> */}
+        {/* <button className="btn btn-secondary btn-sm">Increment</button> */}
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </div>
     );
   }
 
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
-  }
+  // getBadgeClasses() {
+  //   let classes = "badge m-2 badge-";
+  //   classes += this.state.count === 0 ? "warning" : "primary";
+  //   return classes;
+  // }
+
+  // formatCount() {
+  //   const { count } = this.state;
+  //   return count === 0 ? "Zero" : count;
+  // }
 }
 
 export default Counter;
--- my-first-app/src/index.js
+++ my-first-app/src/index.js
@@ -5,5 +5,6 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import Counter from './components/counter'
+import Counter2 from './components/counter2';
 
-ReactDOM.render(<Counter />, document.getElementById('root'));
+ReactDOM.render(<Counter2 />, document.getElementById('root'));
