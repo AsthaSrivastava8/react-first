39c4ae0c37a3686fae654040f089358a96c53f4a
Astha <srivastavastha811@gmail.com>
2021-04-18 21:33:18 +0530

    Tags added

--- my-first-app/src/index.js
+++ my-first-app/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Children } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -6,5 +6,7 @@ import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import Counter from './components/counter'
 import Counter2 from './components/counter2';
+import PropTypes from 'prop-types';
 
-ReactDOM.render(<Counter2 />, document.getElementById('root'));
+ ReactDOM.render(<Counter2 />, document.getElementById('root'));
+
